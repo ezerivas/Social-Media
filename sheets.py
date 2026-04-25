01ce46ec5765e2d55a0048d3fb74ecb7a6044d9f
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-25 04:43:56 -0300

    fix: scopes2

--- sheets.py
+++ sheets.py
@@ -9,20 +9,20 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-# 👇 leer JSON desde variable de entorno
-creds_json = json.loads(os.getenv("GOOGLE_CREDENTIALS_JSON"))
+def get_sheet():
+    creds_json = json.loads(os.getenv("GOOGLE_CREDENTIALS_JSON"))
 
-creds = Credentials.from_service_account_info(
-    creds_json,
-    scopes=SCOPES
-)
+    creds = Credentials.from_service_account_info(
+        creds_json,
+        scopes=SCOPES
+    )
 
-client = gspread.authorize(creds)
-
-sheet = client.open("mensajes_bot").sheet1
+    client = gspread.authorize(creds)
+    return client.open("mensajes_bot").sheet1
 
 
 def guardar_mensaje(user_id, text):
+    sheet = get_sheet()
     sheet.append_row([
         str(datetime.now()),
         user_id,
