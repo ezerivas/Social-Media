a6993304f4a7be0bdbab6e9324dbead77b715209
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-27 20:22:22 -0300

    Fix DB

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
-DATABASE_URL = os.getenv("${{ Postgres.DATABASE_URL }}")
+DATABASE_URL = os.getenv("DATABASE_URL")
