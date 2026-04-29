879a7b5f727ccc43a492d250ec6a5540e9c0dec3
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-29 08:55:07 -0300

    add

--- app/channels/base.py
+++ app/channels/base.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
-class IBaseChannel(ABC):
+class BaseChannel(ABC):
     @abstractmethod
-    async def send_text(self, to_external_id: str, message_text: str, config: dict) -> str:
+    async def send_message(self, recipient_id: str, message_text: str, access_token: str):
+        """
+        Método obligatorio para enviar mensajes a través de cualquier canal.
+        """
         pass
