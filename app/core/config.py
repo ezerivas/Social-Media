c79c6f6180257c1272259e204a9a3e99265b49a4
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-29 23:12:37 -0300

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,7 @@
-import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # El nombre a la izquierda es como lo usas en Python
-    # El nombre en os.getenv es como debe llamarse en el Dashboard de Railway
-    FB_VERIFY_TOKEN: str = os.getenv("FACEBOOK_VERIFY_TOKEN", "FACEBOOK_TOKEN")
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    FACEBOOK_VERIFY_TOKEN: str = "FACEBOOK_TOKEN"
+    DATABASE_URL: str
 
 settings = Settings()
