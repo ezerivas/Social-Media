7c0de5043875e9e9f42fe671cff72378b1f0316a
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-29 22:14:44 -0300

    fix

--- app/api/webhooks/facebook.py
+++ app/api/webhooks/facebook.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, HTTPException, Depends
+from fastapi import APIRouter, Request, HTTPException, Depends, Querys
 from app.repositories.messages import MessageRepository
 import os
 
