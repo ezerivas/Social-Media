a8b9dfc71b2fd0f9ee1d64d9083d824648296cfb
Ezequiel Rivas <ezequielrivas.15@gmail.com>
2026-04-29 23:13:26 -0300

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     FACEBOOK_VERIFY_TOKEN: str = "FACEBOOK_TOKEN"
     DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
